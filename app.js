7631b8e9090f08c2cdea0a1cfc1397f157b28fee
vladuka6 <vladuka6@gmail.com>
2026-03-17 22:34:00 +0200

    Point bps05 to new D1 API

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 =========================== */
 const LS_KEY = "planner_prototype_ua_v2_full";
 const THEME_KEY = "planner_theme_pref";
-const SYNC_URL = "https://sync.usplan.space/sync";
+const SYNC_URL = "https://api.bps05.fun/sync";
 const SYNC_POLL_MS = 30000;
 const SYNC_DEBOUNCE_MS = 2500;
 const DEVICE_ID_KEY = "planner_device_id";
@@ -8194,3 +8194,4 @@ applyTheme(UI.theme);
 render();
 initAutoSync();
 initOverdueTicker();
+
